768b8467e16e3d9dfdb80ce7312dc6dabb021f94
azibabc121 <azibabc121@gmail.com>
2024-08-06 18:08:55 +0500

    Basics of hoisting

--- Hoisting/README.md
+++ Hoisting/README.md
@@ -1,4 +1,15 @@
 What is hoisting ?
- Ans :
+ Answer :
  Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of their containing scope during the compilation phase, before the code execution begins. This means that you can use variables and functions before they are actually declared in the code. However, only the declarations are hoisted, not the initializations.
- 
+
+1. Variable Hoisting:
+
+When variables are declared using var, their declaration is hoisted to the top of their function or global scope. However, their initialization remains in place.
+
+Variables declared with let and const are also hoisted, but they are not initialized until the code execution reaches their declaration. This creates a "temporal dead zone" from the start of the block until the declaration is encountered.
+
+2. Function Hoisting:
+
+Function declarations are completely hoisted, meaning both the function name and its definition are moved to the top of their scope.
+
+Function expressions, however, are treated like variable assignments and only the variable declaration is hoisted, not the function definition.
--- Hoisting/index.js
+++ Hoisting/index.js
@@ -1,4 +1,19 @@
-/* What is hoisting ?
- Ans :
- Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of their containing scope during the compilation phase, before the code execution begins. This means that you can use variables and functions before they are actually declared in the code. However, only the declarations are hoisted, not the initializations.
- */
+console.log(myName) // Hoisted but not initialized - undefined in console
+
+var myName = "Azib Rasheed"
+
+console.log(myEmail) // ReferenceError: Cannot access 'myEmail' before initialization - creates a "temporal dead zone" from the start of the block until the declaration is encountered.
+let myEmail = "azibabc121@gmail.com"
+
+console.log(myLinkedin) // ReferenceError: Cannot access 'myLinkedin' before initialization - creates a "temporal dead zone" from the start of the block until the declaration is encountered.
+const myLinkedin = "https://www.linkedin.com/in/azib-rasheed"
+
+getMyEmail() // Function declaration & functin definition both are hoisted & it will work
+function getMyEmail() {
+    console.log(myEmail)
+}
+
+getMyLinkedin() // TypeError: getMyLinkedin is not a function because function definition is not hoisted only it's declaration is hoisted with undefined
+var getMyLinkedin = function () {
+    console.log(myLinkedin)
+}
